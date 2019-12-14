5e15b589f750a3c697cac1105e8d8a3eb2605466
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-14 18:57:19 +0100

    added body to class SimilarElement

--- src/com/cursor/hw6/similarity/SimilarElement.java
+++ src/com/cursor/hw6/similarity/SimilarElement.java
@@ -1,6 +1,24 @@
 package com.cursor.hw6.similarity;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SimilarElement<T extends Comparable<T>> {
+    private List<T> firstList;
+    private List<T> secondList;
 
+    public SimilarElement(List<T> firstList, List<T> secondList) {
+        this.firstList = firstList;
+        this.secondList = secondList;
+    }
+
+    public void checkOutSimilarity() {
+        Set<T> intersection = firstList.stream().filter(secondList::contains).collect(Collectors.toSet());
+        if (intersection.size() > 0) {
+            System.out.println("Similar elements: " + intersection.toString());
+        } else {
+            System.out.println("There are no similar items between the two lists");
+        }
+    }
 }
