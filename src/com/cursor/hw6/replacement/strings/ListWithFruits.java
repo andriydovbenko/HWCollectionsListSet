95cc439263424a12fe905e9f2798b6ab0f29f310
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-14 17:25:31 +0100

    created class ListWithFruits

---
+++ src/com/cursor/hw6/replacement/strings/ListWithFruits.java
@@ -0,0 +1,13 @@
+package com.cursor.hw6.replacement.strings;
+
+import java.util.List;
+
+public class ListWithFruits {
+    private List<String> listOfFruits;
+    private String valueToReplace = "Orange";
+    private String valueAfterReplace = "Grapefruit";
+
+    public ListWithFruits(List<String> fruits) {
+        this.listOfFruits = fruits;
+    }
+}
