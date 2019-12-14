b366bc73adca1fe10db48a06ed0f908db399fb4b
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-14 17:23:10 +0100

    added methods replaceDigit() and printList()

--- src/com/cursor/hw6/replacement/digits/ListWithIntegers.java
+++ src/com/cursor/hw6/replacement/digits/ListWithIntegers.java
@@ -16,4 +16,26 @@ public class ListWithIntegers {
         }
         System.out.println("List with random values has created:\n" + listOfDigit + "\n");
     }
+
+    public void replaceDigit() {
+        if (listOfDigit.isEmpty()) {
+            System.out.println("List of Integers is empty");
+        } else {
+            for (int i = 0; i < listOfDigit.size(); i++) {
+                Integer temp = listOfDigit.get(i);
+                if (temp % 3 == 0) {
+                    System.out.println("Digit removed: " + temp);
+                    listOfDigit.remove(temp);
+                }
+            }
+        }
+    }
+
+    public void printList() {
+        if (listOfDigit.isEmpty()) {
+            System.out.println("List of Integers is empty");
+        } else {
+            System.out.println("\nafter removing size = " + listOfDigit.size() + "\n" + listOfDigit + "\n");
+        }
+    }
 }
