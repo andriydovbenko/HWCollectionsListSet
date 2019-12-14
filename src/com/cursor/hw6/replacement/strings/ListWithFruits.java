ae1be1c477c2686baa8c265e1de1251d551f74d7
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-14 17:26:33 +0100

    added method replaceOrangeToGrapefruit()

--- src/com/cursor/hw6/replacement/strings/ListWithFruits.java
+++ src/com/cursor/hw6/replacement/strings/ListWithFruits.java
@@ -10,4 +10,20 @@ public class ListWithFruits {
     public ListWithFruits(List<String> fruits) {
         this.listOfFruits = fruits;
     }
+
+    public String replaceOrangeToGrapefruit() {
+        String message;
+        if (listOfFruits.isEmpty()) {
+            message = "\nList of fruits is empty";
+        } else if (listOfFruits.contains(valueToReplace)) {
+            while (listOfFruits.contains(valueToReplace)) {
+                listOfFruits.set(listOfFruits.indexOf(valueToReplace), valueAfterReplace);
+                System.out.println(valueToReplace + " has transformed to " + valueAfterReplace);
+            }
+            message = "\nafter replace: " + listOfFruits.toString();
+        } else {
+            message = valueToReplace + "\ndoesn't present inside of this list";
+        }
+        return message;
+    }
 }
