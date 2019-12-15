4e6015c5f05c64622c64cf20d055a26d5dc37dc9
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-15 13:56:56 +0100

    enhanced security by creating enum

--- src/com/cursor/hw6/Execution.java
+++ src/com/cursor/hw6/Execution.java
@@ -1,5 +1,6 @@
 package com.cursor.hw6;
 
+import com.cursor.hw6.replacement.objects.Fruit;
 import com.cursor.hw6.replacement.strings.ListWithFruits;
 import com.cursor.hw6.replacement.digits.ListWithIntegers;
 import com.cursor.hw6.similarity.SimilarElement;
@@ -8,6 +9,11 @@ import com.cursor.hw6.sorting.StringInDESC;
 import java.util.*;
 
 public class Execution {
+    private final String APPLE = ListOfFruits.Apple.getName();
+    private final String ORANGE = ListOfFruits.Orange.getName();
+    private final String GRAPEFRUIT = ListOfFruits.Grapefruit.getName();
+    private final String NECTARINE = ListOfFruits.Nectarine.getName();
+    private final String BERRY = ListOfFruits.Berry.getName();
 
     public void startProject() {
         System.out.println(" Task 1");
@@ -18,12 +24,12 @@ public class Execution {
 
         System.out.println(" Task 2");
         List<String> fruits = new ArrayList<>();
-        fruits.add("Apple");
-        fruits.add("Orange");
-        fruits.add("Grapefruit");
-        fruits.add("Nectarine");
-        fruits.add("Berry");
-        fruits.add("Orange");
+        fruits.add(APPLE);
+        fruits.add(ORANGE);
+        fruits.add(GRAPEFRUIT);
+        fruits.add(NECTARINE);
+        fruits.add(BERRY);
+        fruits.add(ORANGE);
         ListWithFruits listWithFruits = new ListWithFruits(fruits);
         System.out.println(listWithFruits.replaceOrangeToGrapefruit());
 
@@ -33,17 +39,18 @@ public class Execution {
         SimilarElement<Integer> stringSimilarElement = new SimilarElement<>(listInteger1, listInteger2);
         stringSimilarElement.checkOutSimilarity();
 
-        List<String> listString1 = new ArrayList<>(Arrays.asList("Apple", "Orange", "Grapefruit", "Berry"));
-        List<String> listString2 = new ArrayList<>(Arrays.asList("Nectarine", "Berry"));
+        List<String> listString1 = new ArrayList<>(Arrays.asList(APPLE,ORANGE,GRAPEFRUIT,BERRY));
+        List<String> listString2 = new ArrayList<>(Arrays.asList(NECTARINE,BERRY));
         SimilarElement<String> stringSimilarElement1 = new SimilarElement<>(listString1, listString2);
         stringSimilarElement1.checkOutSimilarity();
 
         System.out.println("\n Task 4");
         TreeSet<String> treeSet = new TreeSet<>(new StringInDESC());
-        treeSet.add("Nectarine");
-        treeSet.add("Apple");
-        treeSet.add("Orange");
-        treeSet.add("Berry");
+        treeSet.add(NECTARINE);
+        treeSet.add(APPLE);
+        treeSet.add(ORANGE);
+        treeSet.add(BERRY);
         System.out.println("TreeSet in descending order:\n" + treeSet);
+        
     }
 }
