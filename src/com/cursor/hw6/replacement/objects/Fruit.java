72857600ea2e8220cadd1d2d704d9f204db7d0b2
andriydovbenko <andriydovbenko1@gmail.com>
2019-12-15 17:27:18 +0100

    created file FruitsSubstitute to replace object Orange

--- src/com/cursor/hw6/replacement/objects/Fruit.java
+++ src/com/cursor/hw6/replacement/objects/Fruit.java
@@ -1,14 +1,30 @@
 package com.cursor.hw6.replacement.objects;
 
+import com.cursor.hw6.enums.ListOfFruits;
+
 public class Fruit {
-    private int id = 1000;
+    private final String FRUIT = ListOfFruits.Orange.getName();
+    private static int counter = 1000;
+    private int id;
     private String name;
     private String color;
 
+
     public Fruit(String[] configOfFruit) {
-        this.id++;
+        id = counter++;
         this.name = configOfFruit[0];
         this.color = configOfFruit[1];
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
